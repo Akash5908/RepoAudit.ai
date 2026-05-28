1ef86f2d39e1bedd2b5b2e886ca676b2d3a93548
Akash Kumar <venomkr020@gmail.com>
2026-05-28 15:00:00 +0530

    feat: implement FileDropzone component with validation and update dependencies

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,8 @@
+"use client"
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@/components/ui/button";
 import styles from "./page.module.css";
+import { FileDropzone } from "@/components/drop-zone/drop-zone";
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
@@ -23,15 +25,16 @@ export default function Home() {
     <div className="w-screen h-screen " >
       {/* Container */}
       <main className="flex  w-full h-full flex-col justify-center items-center">
-        <div className="flex justify-center h-auto mb-[0vh]">
+        <div className="flex justify-center h-auto ">
           <h1>Drop your project to Start!</h1>
         </div>
         {/* Dropzone  */}
-        <div className="w-[50vw] h-[50vh] bg-blue-500 rounded-md flex justify-center items-center">
+        {/* <div className="w-[50vw] h-[50vh] bg-blue-500 rounded-md flex justify-center items-center">
           <Button size="lg" className="border border-white rounded-md cursor-pointer w-[10vw] bg-slate-600">
             <span className="text-xl px-[10vw]">Drop .zip</span>
           </Button>
-        </div>
+        </div> */}
+        <FileDropzone onFileAccepted={(file) => console.log(file)} />
       </main>
     </div>
   );
