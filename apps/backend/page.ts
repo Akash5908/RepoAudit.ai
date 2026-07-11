e9af11f8364f6622daea5812ec86a6a3512312cf
Akash5908 <venomkr020@gmail.com>
2026-07-11 10:10:00 +0530

    feat: add file upload router with multer integration for handling file uploads

--- apps/backend/page.ts
+++ apps/backend/page.ts
@@ -1,6 +1,15 @@
 import express from "express";
+import { fileUploadRouter } from "./router/file-upload.router";
 
-const app = express.Router();
+const app = express();
 app.use(express.json());
 
-app.use("/v1/api/file-upload");
+app.use("/v1/api/file-upload", fileUploadRouter);
+
+function startServer() {
+  app.listen(5000, () => {
+    console.log("Server started on port 5000");
+  });
+}
+
+startServer();
---
+++ apps/backend/router/file-upload.router.tsx
@@ -0,0 +1,15 @@
+import express, { Router } from "express";
+import multer from "multer";
+
+const router: Router = express.Router();
+const upload = multer({ dest: "./public/data/uploads/" });
+
+/** Will define a post endpoint
+ * It will receive the file and use multer to store in vps /tmp/uploads folder
+ * And will also create a JobID using a UUID
+ */
+router.post("/", upload.single("uploaded_file"), (req, res) => {
+  console.log(req.file, req.body);
+});
+
+export { router as fileUploadRouter };
